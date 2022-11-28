819a0409ab14e43b183641291faf6975230b1c9c
hee <cmlgml094356@gmail.com>
2022-11-28 19:22:16 +0900

    update: diary response에 like 상태와 개수 필드 추가

--- src/main/java/com/swith/backend/domain/diary/presentation/dto/response/DiaryResponse.java
+++ src/main/java/com/swith/backend/domain/diary/presentation/dto/response/DiaryResponse.java
@@ -10,13 +10,17 @@ public class DiaryResponse {
 	private final String content;
 	private final String path;
 	private final Boolean isStatus;
+	private final long like;
+	private final Boolean likeStatus;
 
 	@Builder
-	public DiaryResponse(String title, String content, String path, Boolean isStatus) {
+	public DiaryResponse(String title, String content, String path, Boolean isStatus, long like, Boolean likeStatus) {
 		this.title = title;
 		this.content = content;
 		this.path = path;
 		this.isStatus = isStatus;
+		this.like = like;
+		this.likeStatus = likeStatus;
 	}
 
 }
--- src/main/java/com/swith/backend/domain/diary/service/DiaryService.java
+++ src/main/java/com/swith/backend/domain/diary/service/DiaryService.java
@@ -1,12 +1,16 @@
 package com.swith.backend.domain.diary.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.swith.backend.domain.auth.service.util.AuthUtil;
 import com.swith.backend.domain.diary.domain.Diary;
+import com.swith.backend.domain.like.domain.Like;
 import com.swith.backend.domain.diary.domain.repository.DiaryRepository;
+import com.swith.backend.domain.like.domain.repository.LikeRepository;
 import com.swith.backend.domain.diary.presentation.dto.request.SaveDiaryRequest;
 import com.swith.backend.domain.diary.presentation.dto.request.UpdateDiaryRequest;
 import com.swith.backend.domain.diary.presentation.dto.response.DiaryResponse;
@@ -15,21 +19,23 @@ import com.swith.backend.global.exception.DiaryNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
-@Service
 @Transactional
 @RequiredArgsConstructor
+@Service
 public class DiaryService {
 
 	private final DiaryRepository diaryRepository;
+	private final LikeRepository likeRepository;
 	private final AuthUtil authUtil;
 	private final S3Util s3Util;
 
 	public Long saveDiary(SaveDiaryRequest request, MultipartFile file) {
 		return diaryRepository.save(Diary.builder()
-				.title(request.getTitle())
-				.content(request.getContent())
-				.path(s3Util.uploadImage("image", file))
-				.userId(authUtil.getUser().getId())
+			.title(request.getTitle())
+			.content(request.getContent())
+			.path(s3Util.uploadImage("image", file))
+			.status(request.getStatus())
+			.userId(authUtil.getUser().getId())
 			.build()).getId();
 	}
 
@@ -52,11 +58,14 @@ public class DiaryService {
 
 	public DiaryResponse getDiary(Long diaryId) {
 		Diary diary = diaryRepository.findById(diaryId).orElseThrow(() -> DiaryNotFoundException.EXCEPTION);
+		Boolean likeStatus = likeRepository.existsByIdAndDiaryId(authUtil.getUser().getId(), diaryId);
 
 		return DiaryResponse.builder()
 			.title(diary.getTitle())
 			.content(diary.getContent())
 			.path(s3Util.getS3ObjectUrl(diary.getPath()))
+			.like(likeRepository.countByDiaryId(diaryId))
+			.likeStatus(likeStatus)
 			.build();
 	}
 

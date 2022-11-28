1518fdad43c03542416482049fccf58f0d115ba6
hee <cmlgml094356@gmail.com>
2022-11-28 19:17:55 +0900

    refactor: 파일 위치 이동

--- src/main/java/com/swith/backend/domain/diary/domain/Status.java
+++
@@ -1,5 +0,0 @@
-package com.swith.backend.domain.diary.domain;
-
-public enum Status {
-	PUBLIC, PRIVATE, FRIEND;
-}
---
+++ src/main/java/com/swith/backend/domain/diary/domain/type/Status.java
@@ -0,0 +1,5 @@
+package com.swith.backend.domain.diary.domain.type;
+
+public enum Status {
+	PUBLIC, PRIVATE, FRIEND;
+}
